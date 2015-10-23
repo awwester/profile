dd0802ca18250b5473dad31cfe11e49196332afc
Adam Wester <awwester@gmail.com>
2015-10-23 17:15:04 -0500

    blog on home page

--- core/views.py
+++ core/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
-# Create your views here.
+from blog.models import Article, Tag
+
+
 class HomeView(TemplateView):
     template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        context['tag_list'] = Tag.objects.all()
+        context['article_count'] = Article.objects.count()
+        context['article_list'] = Article.objects.all()[:2]
+
+        return context
