4f07b3ebb337b3788ebed6da2a0c2143fdcb7715
Adam Wester <awwester@gmail.com>
2015-10-24 20:37:53 -0500

    send contact email

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url, patterns
 
-from .views import HomeView
+from .views import HomeView, contact
 
 
 urlpatterns = patterns('',
     url(r'^$', HomeView.as_view(), name="home"),
+    url(r'^contact/$', contact, name="contact"),
 )
--- core/views.py
+++ core/views.py
@@ -1,3 +1,5 @@
+from django.core.mail import send_mail
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
@@ -14,3 +16,20 @@ class HomeView(TemplateView):
         context['article_list'] = Article.objects.all()[:2]
 
         return context
+
+
+def contact(request):
+    "receive a contact AJAX request and try to send an email"
+    if request.method == "POST":
+        try:
+            send = send_mail('New Message Received from ' + request.POST.get('first_name') + '' + request.POST.get('last_name'),
+                request.POST.get('message'), request.POST.get('email'), ['awwester@gmail.com'], fail_silently=False)
+
+            if send == 1:
+                return HttpResponse()
+            else:
+                return HttpResponse(status=400)
+        except:
+            return HttpResponse(status=400)
+    else:
+        return HttpResponse('this is only for AJAX Posts')
