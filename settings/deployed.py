e49dd007ae45292030dfc34c3931b5db72a802d8
Adam Wester <awwester@gmail.com>
2015-10-26 11:07:19 -0500

    switch to sqlite

--- settings/deployed.py
+++ settings/deployed.py
@@ -1,13 +1,8 @@
 from .local import *
-import dj_database_url
 
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-DATABASES = {}
-DATABASES['default'] =  dj_database_url.config()
-DATABASES['default']['CONN_MAX_AGE'] = 500
 
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
