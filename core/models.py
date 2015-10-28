026105bb576110cbb9d2bb90830044105e764531
Adam Wester <awwester@gmail.com>
2015-10-27 22:59:19 -0500

    email subscribe model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class EmailSubscribe(models.Model):
+    email = models.EmailField()
+    signup_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.email
