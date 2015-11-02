a94d9227eaec9726dfbe0e21a9515a622bf1743b
Adam Wester <awwester@gmail.com>
2015-11-02 12:29:53 -0500

    sitemap

--- adam/urls.py
+++ adam/urls.py
@@ -2,9 +2,22 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.sitemaps import GenericSitemap
+from django.contrib.sitemaps.views import sitemap
 
+from blog.models import Article
+
+
+info_dict = {
+    'queryset': Article.objects.all(),
+    'date_field': 'created',
+}
 
 urlpatterns = [
+    url(r'^sitemap\.xml$', sitemap,
+        {'sitemaps': {'blog': GenericSitemap(info_dict, priority=0.8)}},
+        name='django.contrib.sitemaps.views.sitemap'),
+
     url(r'^admin/', include(admin.site.urls)),
     url(r'^blog/', include('blog.urls')),
     url(r'^', include('core.urls')),
