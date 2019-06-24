8b561c6eeba036aea35feafb2a632c4de1df043a
Adam Wester <awwester@gmail.com>
2019-06-24 11:51:57 -0500

    Add bucketeer settings.

--- settings/deployed.py
+++ settings/deployed.py
@@ -5,3 +5,10 @@ django_heroku.settings(locals())
 
 # overwrite the django_heroku staticfiles
 STATICFILES_STORAGE = "django_s3_storage.storage.StaticS3Storage"
+
+
+AWS_S3_BUCKET_NAME = os.environ.get('BUCKETEER_BUCKET_NAME')
+AWS_S3_BUCKET_NAME_STATIC = AWS_S3_BUCKET_NAME
+AWS_REGION = "us-east-1"
+AWS_ACCESS_KEY_ID = os.environ.get('BUCKETEER_AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('BUCKETEER_AWS_SECRET_ACCESS_KEY')
