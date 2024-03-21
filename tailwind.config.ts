3359ad6e0661e34db940f8c5fbb9177484e70be9
Adam Wester <awwester@gmail.com>
2024-03-21 12:25:50 -0500

    Add safelist for dynamic classes.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  safelist: ['text-green-500', 'text-red-500'],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
